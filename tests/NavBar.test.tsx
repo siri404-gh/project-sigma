d2927bb84891ab4c93133ad39cce2d418d7ee6aa
Sreeram Padmanabhan <sreeram.com@gmail.com>
2021-10-25 13:07:55 +0000

    fix: test fix

--- tests/NavBar.test.tsx
+++ tests/NavBar.test.tsx
@@ -5,12 +5,10 @@ import { render, screen } from '@testing-library/react'
 import NavBar from '@/components/NavBar'
 
 describe('NavBar', () => {
-    test('Should render title', () => {
-        render(<NavBar title='web dev' />)
-        screen.logTestingPlaygroundURL();
-        const title = screen.getByText(/web dev/i)
-        // console.log(title)
-        // expect(title).toHaveTextContent('web dev')
-    })
+  test('Should render title', () => {
+    render(<NavBar title='web dev' />)
+    const titleDiv = screen.getByText(/web dev/i)
+    expect(titleDiv).toHaveTextContent('web dev')
+  })
 })
 
