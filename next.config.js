e6d9b92fcff082e438096f30793d5acaa0b2a11e
Sreeram Padmanabhan <sreeram.com@gmail.com>
2021-11-09 18:29:52 +0000

    chore: ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -53,6 +53,11 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: false,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
