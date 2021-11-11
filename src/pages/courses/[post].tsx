84f3ea8ac158fb154dd8d484130c91ba467e3e37
Sreeram Padmanabhan <sreeram.com@gmail.com>
2021-11-11 09:53:46 +0000

    fix: post fix

--- src/pages/courses/[post].tsx
+++ src/pages/courses/[post].tsx
@@ -1,25 +1,31 @@
 import React from 'react'
 
-import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
 
 import Markdown from '@/components/Markdown/Markdown'
-import { fetchPost } from '@/utils/fetchers'
-import { withPemissionsRequired } from '@/utils/hooks'
+import { fetchPost, fetchUserData } from '@/utils/fetchers'
 
 export default ({ data }: { data: string }) => <Markdown>{data}</Markdown>
 
-const _getServerSideProps = async ({ params }: { params: any }) => {
-  const data = await fetchPost('courses', params.post)
-  console.log('data', data)
-  return {
-    props: {
-      data,
-    },
-  }
-}
-
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: withPemissionsRequired({
-    getServerSideProps: _getServerSideProps,
-  }),
+  async getServerSideProps(ctx) {
+    const session = getSession(ctx.req, ctx.res)
+
+    const { tier } = await fetchUserData(session?.user.sub)
+    console.log('tier', tier)
+    if (tier !== '1')
+      return {
+        redirect: {
+          destination: '/premium',
+          permanent: false,
+        },
+      }
+
+    const data = await fetchPost('courses', ctx.params?.post?.toString())
+    return {
+      props: {
+        data,
+      },
+    }
+  },
 })
