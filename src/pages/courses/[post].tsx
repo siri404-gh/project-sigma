6f30c51496f16e527fe6f7e74fef668d9f2212b8
Sreeram Padmanabhan <sreeram.com@gmail.com>
2021-11-11 12:10:01 +0000

    fix: post

--- src/pages/courses/[post].tsx
+++ src/pages/courses/[post].tsx
@@ -9,28 +9,28 @@ export default ({ data }: { data: string }) => <Markdown>{data}</Markdown>
 
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps(ctx) {
-    const session = getSession(ctx.req, ctx.res)
-    if (!session)
-      return {
-        redirect: {
-          destination: '/login',
-          permanent: false,
-        },
-      }
-    const { tier } = await fetchUserData(session?.user.sub)
-    console.log('tier', tier)
-    if (tier !== '1')
-      return {
-        redirect: {
-          destination: '/premium',
-          permanent: false,
-        },
-      }
+    // const session = getSession(ctx.req, ctx.res)
+    // if (!session)
+    //   return {
+    //     redirect: {
+    //       destination: '/login',
+    //       permanent: false,
+    //     },
+    //   }
+    // const { tier } = await fetchUserData(session?.user.sub)
+    // console.log('tier', tier)
+    // if (tier !== '1')
+    //   return {
+    //     redirect: {
+    //       destination: '/premium',
+    //       permanent: false,
+    //     },
+    //   }
 
-    const data = await fetchPost('courses', ctx.params?.post?.toString())
+    // const data = await fetchPost('courses', ctx.params?.post?.toString())
     return {
       props: {
-        data,
+        data: '',
       },
     }
   },
