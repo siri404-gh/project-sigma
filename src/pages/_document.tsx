28a8e6b4acdb1ed426b6eabf39cca9f523d096ea
Sreeram Padmanabhan <sreeram.padmanabhan@team.bumble.com>
2021-11-16 20:06:26 +0000

    chore: head fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 
-import Document, { Html, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <body>
+          <Head />
           <Main />
           <NextScript />
         </body>
