dd9d45ed36f5949d23a9eac162874389be50b154
Sreeram Padmanabhan <sreeram.padmanabhan@team.bumble.com>
2021-11-16 14:17:55 +0000

    chore: seo update

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -263,7 +263,7 @@ const Seo: FC<SEOProps> = ({
       as: 'script',
       args: {
         src: 'https://www.googletagmanager.com/gtag/js?id=' + gtmId,
-        async: true,
+        async: 'true',
       },
       show: !!gtmId,
     },
