f3d7a5f57bf0723a7c9112c8dfd37d3c2d6c4934
Sreeram Padmanabhan <sreeram.padmanabhan@team.bumble.com>
2021-11-22 00:42:02 +0000

    feat: no ssg

--- src/pages/[slug]/[post].tsx
+++ src/pages/[slug]/[post].tsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react'
 
-import { GetStaticProps } from 'next'
+import { getSession } from '@auth0/nextjs-auth0'
+import isbot from 'isbot'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
 import Markdown from '@/components/Markdown/Markdown'
 import config from '@/config'
-import { postUrl, fetchNavlinks } from '@/utils/fetchers'
-import { flatLinks, getPathsSlugPost } from '@/utils/helpers'
+import { postUrl, fetchNavlinks, fetchUserData } from '@/utils/fetchers'
 import { useScroll } from '@/utils/hooks'
 
 const Post = ({
@@ -58,49 +59,114 @@ const Post = ({
 
 export default Post
 
-export async function getStaticPaths() {
-  const navlinks = await fetchNavlinks()
-  const flatNavLinks = flatLinks(
-    navlinks,
-    'links',
-    'url',
-    1,
-    ({ title }) => title !== 'Interview',
-  )
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  req,
+  res,
+}) => {
+  const { post, slug } = params as { post: string; slug: string }
+  const isBot = isbot(req.headers['user-agent'])
+  const isPaidPost = slug === 'interview'
+  const isPaymentCheckRequired = !isBot && isPaidPost
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const paths = getPathsSlugPost(flatNavLinks)
+  try {
+    if (isPaymentCheckRequired) {
+      const session = getSession(req, res)
+      const returnTo = `/interview/${post}`
 
-  return {
-    paths,
-    fallback: false,
+      if (!session) {
+        return {
+          redirect: {
+            destination: `/api/auth/login?returnTo=${returnTo}`,
+            permanent: false,
+          },
+        }
+      }
+
+      const { tier } = await fetchUserData(session.user.sub)
+
+      if (tier !== '1') {
+        return {
+          redirect: {
+            destination: `/premium?returnTo=${returnTo}`,
+            permanent: false,
+          },
+        }
+      }
+    }
+
+    const _res = await fetch(postUrl(slug, post))
+    const data = await _res.text()
+    const navlinks = await fetchNavlinks()
+    const section = navlinks.links.find(
+      (link: { url: string }) => link.url === `/${slug}`,
+    )
+    const { title, url } = section.links.find(
+      (link: { url: string }) => link.url === `/${slug}/${post}`,
+    )
+
+    return {
+      props: {
+        data,
+        title: `${title} | ${config.seo.title}`,
+        url: process.env.NEXT_PUBLIC_DOMAIN + url,
+      },
+    }
+  } catch (error) {
+    console.error(error)
+
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
   }
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug, post } = params as { slug: string; post: string }
+// export async function getStaticPaths() {
+//   const navlinks = await fetchNavlinks()
+//   const flatNavLinks = flatLinks(
+//     navlinks,
+//     'links',
+//     'url',
+//     1,
+//     ({ title }) => title !== 'Interview',
+//   )
 
-  const navlinks = await fetchNavlinks()
+//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//   // @ts-ignore
+//   const paths = getPathsSlugPost(flatNavLinks)
 
-  const section = navlinks.links.find(
-    (link: { url: string }) => link.url === `/${slug}`,
-  )
-  const index = section.links.findIndex(
-    (link: { url: string }) => link.url === `/${slug}/${post}`,
-  )
+//   return {
+//     paths,
+//     fallback: false,
+//   }
+// }
 
-  const { title, url } = section.links[index]
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const { slug, post } = params as { slug: string; post: string }
 
-  const res = await fetch(postUrl(slug, post))
-  const data = await res.text()
+//   const navlinks = await fetchNavlinks()
 
-  return {
-    props: {
-      data,
-      title,
-      url: process.env.NEXT_PUBLIC_DOMAIN + url,
-    },
-    revalidate: 60 * 60,
-  }
-}
+//   const section = navlinks.links.find(
+//     (link: { url: string }) => link.url === `/${slug}`,
+//   )
+//   const index = section.links.findIndex(
+//     (link: { url: string }) => link.url === `/${slug}/${post}`,
+//   )
+
+//   const { title, url } = section.links[index]
+
+//   const res = await fetch(postUrl(slug, post))
+//   const data = await res.text()
+
+//   return {
+//     props: {
+//       data,
+//       title,
+//       url: process.env.NEXT_PUBLIC_DOMAIN + url,
+//     },
+//     revalidate: 60 * 60,
+//   }
+// }
